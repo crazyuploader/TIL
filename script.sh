a2024940805fa8b0d185c123a624ae4ce77cdf00
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-10-05 13:27:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 ./count.sh
 echo ""
-echo -e "${GREEN}Run Prettier...${NC}"
+echo -e "${YELLOW}Run Prettier...${NC}"
 npx prettier --write .
 git config user.email "49350241+crazyuploader@users.noreply.github.com"
 git config user.name "crazyuploader"
@@ -21,7 +21,7 @@ if [[ -z $(git status --porcelain) ]]; then
     echo -e "${GREEN} Nothing to commit.${NC}"
 else
     git add .
-    git commit -m "Auto Update [skip ci]"
+    git commit -m "Auto Update [skip ci]" -m "" -m "Update TIL number, and run Prettier"
     git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GH_REF}" HEAD:${TRAVIS_BRANCH}
     echo -e "${YELLOW}Updates Pushed to https://${GH_REF}"
 fi
